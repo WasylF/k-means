6e1bd6be0c9ab41a4aed5383031d9273c483af3a
Wsl_F <WasylFranchuk@gmail.com>
2016-03-13 19:43:30 +0200

    Creat Point class

--- Main.cpp
+++ Main.cpp
@@ -18,6 +18,42 @@ typedef pair<LL,LL> pLL;
 #define pb(x)  push_back(x)
 #define sqr(x) ((x)*(x))
 
+class Point
+{
+private:
+
+    dbl x,y,z,v;
+
+public:
+    Point(dbl x1, dbl y1, dbl z1, dbl v1) : x(x1), y(y1), z(z1), v(v1) { }
+
+    Point() : x(0), y(0), z(0), v(0) { }
+
+    dbl dist(Point &p2)
+    {
+        dbl d= 0;
+        d+= sqr(x-p2.x);
+        d+= sqr(y-p2.y);
+        d+= sqr(z-p2.z);
+        d+= sqr(v-p2.v);
+        d= sqrt(d);
+        return d;
+    }
+
+    friend ostream& operator<<(ostream& out, Point &p)
+    {
+        out << p.x << " " << p.y << " " << p.z << " " << p.v << endl;
+        return out;
+    }
+
+    friend istream& operator>>(istream &input, Point &p)
+    {
+        input >> p.x >> p.y >> p.z >> p.v;
+        return input;
+    }
+
+};
+
 
 int main()
 {
