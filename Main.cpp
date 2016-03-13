4484d3f26de878ab4ab7274619aacf2c93ee5d09
Wsl_F <WasylFranchuk@gmail.com>
2016-03-13 19:42:40 +0200

    init

---
+++ Main.cpp
@@ -0,0 +1,26 @@
+//solution by Wsl_F
+#include <bits/stdc++.h>
+
+using namespace std;
+#pragma comment(linker, "/STACK:1024000000,1024000000")
+
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef double dbl;
+typedef vector<int> vi;
+typedef vector<LL> vL;
+typedef vector<string> vs;
+typedef pair<int,int> pii;
+typedef pair<LL,LL> pLL;
+
+#define mp(x,y)  make_pair((x),(y))
+#define pb(x)  push_back(x)
+#define sqr(x) ((x)*(x))
+
+
+int main()
+{
+
+    return 0;
+}
